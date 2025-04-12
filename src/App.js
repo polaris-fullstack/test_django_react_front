3170901ff8fbe961b6e92d104910c4f7ed8a53a6
polaris-fullstack <rayrascoe226@gmail.com>
2025-04-12 12:39:38 +0200

    modify frontend add new data by dialog

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { CssBaseline, ThemeProvider, createTheme, Grid, Paper, Typography, Box } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme, Grid, Paper, Typography, Box, Dialog, DialogTitle, DialogContent, IconButton } from '@mui/material';
 import ContactForm from './components/ContactForm';
 import ContactsTable from './components/ContactsTable';
+import AddIcon from '@mui/icons-material/Add';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const theme = createTheme({
   palette: {
@@ -17,6 +19,7 @@ const theme = createTheme({
 function App() {
   const [contacts, setContacts] = useState([]);
   const [refreshTable, setRefreshTable] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
 
   useEffect(() => {
     fetchContacts();
@@ -35,28 +38,68 @@ function App() {
 
   const handleContactAdded = () => {
     setRefreshTable(!refreshTable);
+    setOpenDialog(false);
+  };
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
   };
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Box sx={{ flexGrow: 1, p: 3 }}>
-        <Typography variant="h4" component="h1" gutterBottom align="center" sx={{ py: 2 }}>
-          Contact Management System
-        </Typography>
-        <Grid container spacing={3} justifyContent="center">
-          <Grid item xs={12} md={6} lg={6}>
-            <Paper elevation={3} sx={{ p: 2 }}>
-              <ContactForm onContactAdded={handleContactAdded} />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={6} lg={6}>
-            <Paper elevation={3} sx={{ p: 2 }}>
-              <ContactsTable contacts={contacts} onContactUpdated={handleContactAdded} />
-            </Paper>
-          </Grid>
-        </Grid>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 3 }}>
+          <Typography variant="h4" component="h1">
+            Contact Management System
+          </Typography>
+          <Box sx={{ display: 'flex', gap: 1 }}>
+            <IconButton 
+              color="primary" 
+              onClick={handleOpenDialog}
+              sx={{ 
+                backgroundColor: 'primary.main',
+                color: 'white',
+                '&:hover': {
+                  backgroundColor: 'primary.dark',
+                }
+              }}
+            >
+              <AddIcon />
+            </IconButton>
+            <IconButton 
+              color="primary" 
+              sx={{ 
+                backgroundColor: 'primary.main',
+                color: 'white',
+                '&:hover': {
+                  backgroundColor: 'primary.dark',
+                }
+              }}
+            >
+              <SettingsIcon />
+            </IconButton>
+          </Box>
+        </Box>
         
+        <Paper elevation={3} sx={{ p: 2 }}>
+          <ContactsTable contacts={contacts} onContactUpdated={handleContactAdded} />
+        </Paper>
+
+        <Dialog 
+          open={openDialog} 
+          onClose={handleCloseDialog}
+          maxWidth="sm"
+          fullWidth
+        >          
+          <DialogContent>
+            <ContactForm onContactAdded={handleContactAdded} />
+          </DialogContent>
+        </Dialog>
       </Box>
     </ThemeProvider>
   );
